44683757f1662d291f81678a793758ab56bf59f7
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2022-11-20 21:50:00 +0600

    learning JavaScript

---
+++ src/components/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../App.css";
+
+import Cards from "../Cards.js";
+import Footer from "../Footer.js";
+import HeroSection from "../HeroSection.js";
+
+
+
+
+const Home = () => {
+  return (
+      <>
+          <HeroSection />
+          <Cards />
+          <Footer />
+      </>
+  );
+};
+
+export default Home;
