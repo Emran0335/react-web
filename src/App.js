3a044fab77e462b6559049efdb7d294ac89f54ab
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2023-02-05 23:26:20 +0600

    travel react-website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home.js";
 import Products from "./components/pages/Products.js";
 import Services from "./components/pages/Services.js";
@@ -10,13 +10,13 @@ function App() {
   return (
     <>
       <Router>
-              <Navbar />
-              <Routes>
-                  <Route path='/' exact element={<Home/>} />
-                  <Route path='/services' element={<Services/>} />
-                  <Route path='/products' element={<Products/>} />
-                  <Route path='/sign-up' element={<SignUp/>} />
-              </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
       </Router>
     </>
   );
